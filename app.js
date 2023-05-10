d3ab9073ba29a2b3edc1001531c738f0db7e096a
serifetoy <92857592+serifetoy@users.noreply.github.com>
2023-05-10 11:07:45 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 import axios from "axios";
 
+// Function to retrieve user data and posts
 const getData = async function getData(user_id) {
     try {
+         // Fetch user data and posts based on the specified user ID
         const { data: users } = await axios("https://jsonplaceholder.typicode.com/users/" + user_id);
         const { data: posts } = await axios("https://jsonplaceholder.typicode.com/posts?userId=" + user_id);
 
+         // Log the retrieved user data and posts to the console
         console.log("USERS UPLOADED", users);
         console.log("POST UPLOADED", posts)
     
     } catch (error) {
+        // Log any errors that occur during the data retrieval process
         console.log(error);
     }
 
 }
 
-
+// Export the getData function as the default export
 export default getData;
